7dab09f493a87bde13a68ba0eaeda159c5b014c7
hmmini <pak4184@naver.com>
2025-01-31 21:00:00 +0900

    스트림 퀴즈 풀이 1.2 각 취미를 선호하는 정씨 성을 갖는 인원이 몇 명인지 계산하여라.

--- src/main/java/com/mangkyu/stream/Quiz1/Quiz1.java
+++ src/main/java/com/mangkyu/stream/Quiz1/Quiz1.java
@@ -34,9 +34,24 @@ public class Quiz1 {
     }
 
     // 1.2 각 취미를 선호하는 정씨 성을 갖는 인원이 몇 명인지 계산하여라.
+
+    /**
+     * 1.1 문제에서 초반 filter을 통해 정씨로 시작하는 성을 찾고 취미를 선호하는 인원을 계산한다.
+     */
     public Map<String, Integer> quiz2() throws IOException, CsvException {
         List<String[]> csvLines = readCsvLines();
-        return new HashMap<>();
+
+        Map<String, Integer> result = csvLines.stream()
+                .filter(arr -> arr[0].startsWith("정"))
+                .map(arr -> arr[1].replace(" ", ""))
+                .flatMap(hobby -> Arrays.stream(hobby.split(":")))
+                .collect(Collectors.toMap(
+                        hobby -> hobby,
+                        hobby -> 1,
+                        (oldValue, newValue) -> newValue += oldValue
+                ));
+
+        return result;
     }
 
     // 1.3 소개 내용에 '좋아'가 몇번 등장하는지 계산하여라.
