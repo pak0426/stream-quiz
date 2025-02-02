3c202e95ec2234544ac64e46a1a2ea7006927a62
hmmini <pak4184@naver.com>
2025-02-02 21:00:00 +0900

    2.1 List에 저장된 단어들의 접두사가 각각 몇개씩 있는지 Map<String, Integer>으로 변환하여라.

--- src/main/java/com/mangkyu/stream/Quiz2/Quiz2.java
+++ src/main/java/com/mangkyu/stream/Quiz2/Quiz2.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Quiz2 {
 
@@ -12,7 +13,23 @@ public class Quiz2 {
     // 2.1 List에 저장된 단어들의 접두사가 각각 몇개씩 있는지 Map<String, Integer>으로 변환하여라.
     // ex) ("T", 1), ("a", 2) ...
     public Map<String, Integer> quiz1() {
-        return new HashMap<>();
+
+        Map<String, Integer> result = WORDS.stream()
+                .collect(Collectors.toMap(
+                        str -> str.substring(0, 1),
+                        str -> {
+                            int count = 0;
+                            for (String word : WORDS) {
+                                if (word.startsWith(str)) {
+                                    count++;
+                                }
+                            }
+                            return count;
+                        },
+                        Integer::max
+                ));
+
+        return result;
     }
 
     // 2.2 List에 저장된 단어들 중에서 단어의 길이가 2 이상인 경우에만, 모든 단어를 대문자로 변환하여 스페이스로 구분한 하나의 문자열로 합한 결과를 반환하여라.
