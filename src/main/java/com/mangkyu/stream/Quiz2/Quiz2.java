73662fab87758c8c8f740eb8300def2d6be98d03
hmmini <pak4184@naver.com>
2025-02-03 21:00:00 +0900

    2.2 List에 저장된 단어들 중에서 단어의 길이가 2 이상인 경우에만, 모든 단어를 대문자로 변환하여 스페이스로 구분한 하나의 문자열로 합한 결과를 반환하여라.

--- src/main/java/com/mangkyu/stream/Quiz2/Quiz2.java
+++ src/main/java/com/mangkyu/stream/Quiz2/Quiz2.java
@@ -35,7 +35,15 @@ public class Quiz2 {
     // 2.2 List에 저장된 단어들 중에서 단어의 길이가 2 이상인 경우에만, 모든 단어를 대문자로 변환하여 스페이스로 구분한 하나의 문자열로 합한 결과를 반환하여라.
     // ex) ["Hello", "a", "Island", "b"] -> “H I”
     public String quiz2() {
-        return null;
+        String result = WORDS.stream()
+                .filter(word -> word.length() > 1)
+                .map(word -> {
+                    word = word.toUpperCase();
+                    word = word.substring(0, 1);
+                    return word;
+                })
+                .collect(Collectors.joining(" "));
+        return result;
     }
 
 }
